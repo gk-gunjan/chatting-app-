77b3cddb5eb8c3a7ac3bf2414d3879de5f773b29
gk-gunjan <gunjan.krr28@gmail.com>
2021-02-06 19:16:12 +0530

    return if message is a imahe or not

--- src/component/MyMessage.jsx
+++ src/component/MyMessage.jsx
@@ -1,7 +1,19 @@
-const MyMessage =()=>{
+const MyMessage =({message})=>{
+    //here we check if message is text or an image
+    if(message?.attachments?.length  >0){
+        return (
+            <img
+                src={message.attachments[0].file}
+                alt="message is in image form"
+                className="message-image"
+                style={{float:'right'}}
+            />
+        )
+    }
+
     return (
-        <div>
-            My Message
+        <div >
+        
         </div>
     );
 }
