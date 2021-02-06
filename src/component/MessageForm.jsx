47704d33a15b3ee980033cf2bce4225040bca4ec
gk-gunjan <gunjan.krr28@gmail.com>
2021-02-06 20:15:50 +0530

    form table in Message form

--- src/component/MessageForm.jsx
+++ src/component/MessageForm.jsx
@@ -1,17 +1,44 @@
-const MyMessage =()=> {
-    const handleSubmit =(e)=>{
-        e.preventDefaults;
+import {useState} from "react";
+import {sendMessage ,isTyping} from 'react-chat-engine';
+import {sendOutlined ,PictureOutlined} from "@ant-design/icons";
+
+const MessageForm =(props)=> {
+    const [value ,setValue]=useState('');
+    const [chatId ,creds]=props;
+
+    const handleSubmit =(event)=>{
+        event.preventDefault();//dont refresh the page
+
+        const text= value.trim();//Removes the leading and trailing white space and line terminator characters from a string.
+
+        if(text.length >0) sendMessage(creds,chatId ,{ text }); //text here means text message
+
         
+        setValue('');
+    }
+
+    const handleChange =(event)=> {
+        setValue(event.target.value);
+
+        isTyping(props,chatId);
     }
     return (
         <form className='message-form' onSubmit={handleSubmit}>
             <input 
                 className="message-input"
-                
+                placeholder="Type Here..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+
+                </span>
+            </label>
 
         </form>
 
     );
 }
-export default MyMessage;
+export default MessageForm;
